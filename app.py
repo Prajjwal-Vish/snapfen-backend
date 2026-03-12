5ca0b674622db303f42a4c6b4c06509a723b9a00
prajjwal <prajjwalvish2@gmail.com>
2026-03-13 01:51:14 +0530

    Added flask cors import

--- app.py
+++ app.py
@@ -348,6 +348,7 @@ load_dotenv()
 # --- IMPORTS ---
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import db, User, Scan 
