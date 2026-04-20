7a9cdd17922a375eb256061b2544d4a9771d4794
Prajjwal <GFG19761@gfg.geeksforgeeks.org>
2026-04-20 16:39:19 +0530

    Adding start.sh file to smartly run celery on render free web service

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# 1. Start the Celery worker in the background (notice the & at the end)
+# NOTE: We use --concurrency=1 because Render's free tier only has 512MB of RAM. 
+# Loading the ML model multiple times will cause an Out of Memory (OOM) crash.
+celery -A tasks.celery_app worker --loglevel=info --concurrency=2 &
+
+# 2. Start the Flask web server in the foreground
+gunicorn app:app --bind 0.0.0.0:$PORT
